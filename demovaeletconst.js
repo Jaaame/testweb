51277905a1c40c88d1707e918db7c29002323794
Jamme <6306021612015@fitm.kmutnb.ac.th>
2022-01-19 15:42:32 +0700

    update

---
+++ demovaeletconst.js
@@ -0,0 +1,16 @@
+var hello = "Hello";
+
+console.log(hello);
+hello = "Hello World";
+console.log(hello);
+
+if(true){
+    let world = "Hello Word";
+    console.log(world);
+}
+// console.log(World)
+
+const fixval = "Fix Value";
+console.log(fixval);
+
+//fixvar = "tes assigne value to const"
