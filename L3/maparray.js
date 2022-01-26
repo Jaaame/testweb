0ccce48245f96167625e4b37c8cc189503c1f38c
Jamme <6306021612015@fitm.kmutnb.ac.th>
2022-01-26 15:08:38 +0700

    update

---
+++ L3/maparray.js
@@ -0,0 +1,7 @@
+const finalParticpants = ["Taylor","Donald","Don","Natasha","Bobboy"];
+
+const announcements = finalParticpants.map((menubar) => {
+    return menubar + " joined the contest.";
+});
+
+console.log(announcements);
